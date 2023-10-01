7bbc52b9558bee8f7d0f7b4590674f9f10b82b32
vjz3qz <vjz3qz@virginia.edu>
2023-10-01 16:47:09 -0400

    set up vectorize route

---
+++ app/routes.py
@@ -0,0 +1,22 @@
+from flask import request, jsonify
+from app.services import preprocessor, tokenizer, vectorizer
+
+def setup_routes(app):
+    @app.route('/vectorize', methods=['POST'])
+    def vectorize():
+        try:
+            # we get the text parameter from the request body
+            data = request.json 
+            text = data.get('text') 
+
+            if not text:
+                return jsonify({'error': 'Missing text'}), 400
+            
+            cleaned_text = preprocessor.clean(text)
+            tokens = tokenizer.tokenize(cleaned_text)
+            vector = vectorizer.vectorize(tokens)
+
+            return jsonify({'vector': vector}), 200
+        
+        except Exception as e:
+            return jsonify({'error': str(e)}), 500
