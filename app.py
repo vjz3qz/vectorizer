6631ebe73a8f64f2590d6a75a18987ce4e2c30e6
vjz3qz <vjz3qz@virginia.edu>
2023-10-01 16:42:47 -0400

    created app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from app.routes import setup_routes
 
-app  = Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello World!'
+# Setup routes
+# setup_routes(app)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
