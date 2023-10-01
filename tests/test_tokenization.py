e4b521de3754e41fbbf8a1f8847c490091e65316
vjz3qz <vjz3qz@virginia.edu>
2023-10-01 18:04:22 -0400

    created tokenization test

--- tests/test_tokenization.py
+++ tests/test_tokenization.py
@@ -1,3 +1,22 @@
 import unittest
-from ..app.tokenization import Tokenizer
+from services import tokenizer
+
+class TestTokenizer(unittest.TestCase):
+    def test_tokenize(self):
+        text = "This is a test"
+        tokens = tokenizer.tokenize(text)
+        self.assertEqual(tokens, ['this', 'is', 'a', 'test'])
+
+    def test_empty_input(self):
+        text = ""
+        tokens = tokenizer.tokenize(text)
+        self.assertEqual(tokens, [])
+    
+    def test_none_input(self):
+        with self.assertRaises(TypeError):
+            tokens = tokenizer.tokenize(None)
+
+if __name__ == '__main__':
+    unittest.main()
+
 
